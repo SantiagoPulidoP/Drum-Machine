0020d35ba0978320034b6bb04537d206977c243d
Santiago Pulido <santiagopulidopelaez@gmail.com>
2020-05-25 23:23:15 -0500

    Sound done, design implementation is pending

--- src/App.js
+++ src/App.js
@@ -1,26 +1,74 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
+import './App.css';
+import {Howl} from 'howler';
+/* import bass from './sounds/bass.mp3'
+import crash from './sounds/crash.mp3'
+import snare from './sounds/snare.mp3'
+ */
+
+const keySounds = [
+  
+  {key: 'q', clip: 'https://www.fesliyanstudios.com/play-mp3/6703', ref:'hi' },
+  {key: 'w', clip: 'https://www.fesliyanstudios.com/play-mp3/6780', ref:'hat'},
+  {key: 'e', clip: 'https://www.fesliyanstudios.com/play-mp3/6773', ref:'snare'},
+  {key: 'a', clip: 'https://www.fesliyanstudios.com/play-mp3/6659', ref:'bass' },
+  {key: 's', clip: 'https://www.fesliyanstudios.com/play-mp3/6735', ref:'bell' },
+  {key: 'd', clip: 'https://www.fesliyanstudios.com/play-mp3/6741', ref: 'ride' },
+  {key: 'z', clip: 'http://dight310.byu.edu/media/audio/FreeLoops.com/3/3/Foxy%2070s%20Guitar%20Hit-1455-Free-Loops.com.mp3', ref: 'delay' },
+  {key: 'x', clip: 'http://www.burnkit2600.com/temp/HR-16/HR-16-WAVs/39-maracas%20a.wav', ref: 'maracas'},
+  {key: 'c', clip: 'http://www.chiptape.com/chiptape/sounds/medium/MidwayGauntletG010.WAV' ,ref: 'piano'}
+];
+
+
+class App extends Component {
+
+  SoundPlay = (src)=>{
+    const sound = new Howl({
+      src,
+      html5: true
+    });
+    sound.play();
+
+  }
+
+  RenderButtonAndSound(){
+    return keySounds.map((sound,index)=>{
+     
+      return (
+        <button key={index} id={sound.ref}>
+        {sound.key.toUpperCase()}
+        </button>
+      )
+    })
+  }
+
+
+  look(e){
+    
+    keySounds.map((sound)=>{
+      if(e.key===sound.key){
+        this.SoundPlay(sound.clip)
+        document.getElementById(sound.ref).style.opacity=0.6
+        setTimeout(() => {
+          document.getElementById(sound.ref).style.opacity=1
+        }, 200);
+      }
+      
+    })
+  }
+
+  render(){
+  {document.onkeydown = this.look.bind(this)}
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Hello everyBody</h2>
+      {this.RenderButtonAndSound()}   
     </div>
   );
+}
+
+
 }
 
 export default App;
